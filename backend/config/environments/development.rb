483f7c804b69b4e5a7253bfa55daca342bf0e14a
Aya <ayaaa.okzk@gmail.com>
2025-12-09 14:04:25 -0800

    add mailtrap credential and setting

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -30,10 +30,25 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
 
   # Make template changes take effect immediately.
   config.action_mailer.perform_caching = false
+
+
+  # mailtrap Setting
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: Rails.application.credentials.dig(:mailtrap, :user_name),
+    password: Rails.application.credentials.dig(:mailtrap, :password),
+    address: "sandbox.smtp.mailtrap.io",
+    port: 587,
+    authentication: :plain,
+    domain: "localhost",
+    enable_starttls_auto: true
+  }
+
 
   # Set localhost to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
