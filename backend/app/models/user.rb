0493e4c613be0d30bd55d0bcd838cb0290ac708d
Aya <ayaaa.okzk@gmail.com>
2025-12-09 14:37:59 -0800

    set devise and devise-jwt

---
+++ backend/app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # The token will be expired once logged out
+  # jit is the json token identifier and unique ID for token. and Devise-JWT uses it for the token revocation strategy to invalidate old or logged-out tokens
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :jwt_authenticatable,
+         jwt_revocation_strategy: self
+end
